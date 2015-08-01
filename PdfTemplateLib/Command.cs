1a33c20caf4fd3efa2230e5a789a023a87d5021a
twotymz <joshua.p.may@gmail.com>
2015-08-01 08:19:18 -0400

    Moving stuff around in Command.cs

--- PdfTemplateLib/Command.cs
+++ PdfTemplateLib/Command.cs
@@ -34,42 +34,5 @@ namespace PdfTemplateLib
 
         public Opcode Code { get; set; }
         public List<string> Args { get; set; }
-
-        /*
-        public int ToInt ()
-        {
-            int v;
-            if (int.TryParse(Args, out v))
-            {
-                return v;
-            }
-
-            return 0;
-        }
-
-        public Rectangle ToRect ()
-        {
-            string[] tokens = Args.Split(' ');
-            if (tokens.Length == 4)
-            {
-                int x, y, w, h;
-                if (int.TryParse(tokens[0], out x))
-                {
-                    if (int.TryParse(tokens[1], out y))
-                    {
-                        if (int.TryParse(tokens[2], out w))
-                        {
-                            if (int.TryParse(tokens[3], out h))
-                            {
-                                return new Rectangle(x, y, w, h);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return new Rectangle(0, 0, 0, 0);
-        }
-         */
     }
 }
